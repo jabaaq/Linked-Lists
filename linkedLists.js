d9b7e23ceae83cf826fd63c3f4334ac588c37bf4
Jaba <jabaqadagishvili4@gmail.com>
2023-10-03 17:02:27 +0400

    Created append the append function

--- linkedLists.js
+++ linkedLists.js
@@ -14,4 +14,26 @@ class LinkedList {
         this.head = null
         this.tail = null
     }
+
+    append(value) {
+        const newNode = new Node(value)
+
+        if (!this.head || !this.tail) {
+            this.head = newNode
+            this.tail = newNode
+
+            return this
+        }
+
+        this.tail.next = newNode            //Next is always looking for null, but i added newNode now
+        this.tail = newNode
+
+        return this         // The current list will be returned
+    }
 }
+
+const list = new LinkedList()
+list.append('a').append('b').append('c')
+
+console.log(JSON.stringify(list));
+//{"head":{"value":"a","next":{"value":"b","next":{"value":"c","next":null}}},"tail":{"value":"c","next":null}}
